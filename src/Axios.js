52d6c94a16624084f09c79c401f93ae6cd5c9197
nikhilkops <nikhil.kops@gmail.com>
2024-01-07 21:16:07 +0530

    Link daal diya hai

--- src/Axios.js
+++ src/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const APILink = process.env.COLLAB_BACKEND_LINK | 'https://code-collab-server.onrender.com/';
+const APILink =  'https://code-collab-server.onrender.com/';
 
 const API = axios.create({baseURL:APILink}) 
 
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-const socketLink = process.env.COLLAB_BACKEND_LINK | 'https://code-collab-server.onrender.com/';
+const socketLink =  'https://code-collab-server.onrender.com/';
 const socket = socketIOClient(socketLink); // Replace with your server URL
 
 export default socket;

0305a273f3be88734e9ff092c61ec73534c33d83
nikhilkops <nikhil.kops@gmail.com>
2024-01-11 22:17:15 +0530

    process env removed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import Logo from '../../images/Logo.svg'
 import './NavBar.css'
 const NavBar = () => { 
-  if(process.env.COLLAB_BACKEND_LINK){
-    console.log(process.env.COLLAB_BACKEND_LINK)
-  }
+   
   return ( 
       <header id="header" className=  "header" > 
           <img

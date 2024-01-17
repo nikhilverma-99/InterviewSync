f02a22440b2d713f0011ca39d18c15d51fda78d2
nikhilkops <nikhil.kops@gmail.com>
2024-01-17 20:32:55 +0530

    Admin Panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext, useContext,lazy,Suspense } from 'react'
+import { useEffect, useState, createContext, useContext,lazy,Suspense  } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import Hero from './components/Hero/Hero'  
 import './App.css'
@@ -9,35 +9,44 @@ const ProblemEditor = lazy(()=>import('./components/Problem+Editor/ProblemEditor
 const Login = lazy(()=>import('./components/Login/Login'))
 import Table from './components/Table/Table'
 // import Error from './components/Error/Error'
+import MaterialTable from './components/Table/MaterialTable'
 import { Route, Routes } from 'react-router-dom'
 import VideoCall from './components/Dyanamic Width Components/VideoCall'
 import HowItWorks from './components/How it Works/HowItWorks'
+import Admin from './components/Admin/Admin'
 // import Pricing from './components/pricing/Pricing'
 const CodeCollabContext = createContext();
+ 
 function App() { 
   const [selectedTheme, setSelectedTheme] = useState('');   
   return (
     <>
-    <CodeCollabContext.Provider value={{selectedTheme,setSelectedTheme}}>
-    <Suspense fallback={<Loading></Loading>}>
-      <Routes>
-        <Route path='/' element={
-          <> 
-            <NavBar></NavBar>
-            <Hero></Hero>
-            <HowItWorks></HowItWorks>
-            {/* <Pricing></Pricing> */}
-            <Editor></Editor>
-            {/* <Error></Error> */}
-          </>}> 
-        </Route>
+      <CodeCollabContext.Provider value={{selectedTheme,setSelectedTheme}}> 
+      <Suspense fallback={<Loading></Loading>}>
+        <Routes>
+          <Route path='/' element={
+            <> 
+              <NavBar></NavBar>
+              <Hero></Hero>
+              <HowItWorks></HowItWorks>
+              {/* <Pricing></Pricing> */}
+              {/* <Editor></Editor> */}
+              {/* <Error></Error> */}
+              {/* <Table></Table>
+              <MaterialTable></MaterialTable> */} 
+              <Admin></Admin>
+            </>}> 
+          </Route>  
 
-        <Route path='/enterInterview' element={<Login></Login>}></Route>
-        <Route path='/videocalling' element={<VideoCall></VideoCall>}></Route> 
-        <Route path='/problemEditor/:type' element={<ProblemEditor></ProblemEditor>}></Route>
-      </Routes>
-      </Suspense> 
-    </CodeCollabContext.Provider> 
+          <Route path='/enterInterview' element={<Login></Login>}></Route>
+          <Route path='/videocalling' element={<VideoCall></VideoCall>}></Route> 
+
+            <Route path='/problemEditor/:type' element={<ProblemEditor></ProblemEditor>}></Route>
+
+        </Routes>
+        </Suspense>  
+      </CodeCollabContext.Provider> 
+
     </>
   )
 }

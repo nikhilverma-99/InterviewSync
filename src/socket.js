87d6911d05edd2e6a396c604943fa20d67b45dd8
nikhilkops <nikhil.kops@gmail.com>
2024-01-21 21:34:44 +0530

    socket corrected

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
 const socketLink = `/api/v1`;
-const socket = socketIOClient(socketLink,{
-    reconnection: true,
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000,  // milliseconds
-    reconnectionDelayMax: 5000,  // milliseconds
-  }); // Replace with your server URL
+const socket = socketIOClient(socketLink); // Replace with your server URL
 
 export default socket;

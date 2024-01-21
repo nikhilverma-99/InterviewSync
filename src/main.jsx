44632cd9cbf174ff981d4080b536802c68561fa7
nikhilkops <nikhil.kops@gmail.com>
2024-01-21 16:59:59 +0530

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx' 
 
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter as Router } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
    
 )

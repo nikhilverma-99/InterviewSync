84456101a9aaf3895212e5e9d1707d2364d9aaeb
nikhilkops <nikhil.kops@gmail.com>
2024-01-21 21:39:43 +0530

    socket corrected

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-const socketLink = `/api/v1`;
+const socketLink = `https://codecollab-k6wq.onrender.com`;
 const socket = socketIOClient(socketLink); // Replace with your server URL
 
 export default socket;

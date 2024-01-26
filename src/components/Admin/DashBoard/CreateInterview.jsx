ff26596ed1b60d9d9f11ffd60a0e7f74918a8377
nikhilkops <nikhil.kops@gmail.com>
2024-01-26 16:36:54 +0530

    Create Interview , added icons

--- src/components/Admin/DashBoard/CreateInterview.jsx
+++ src/components/Admin/DashBoard/CreateInterview.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import   './CreateInterview.css';
-
-
+import { FaIdCard } from "react-icons/fa";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+import { FaRankingStar } from "react-icons/fa6";
+import { MdOutlineContactPage } from "react-icons/md";
+import { CiCalendarDate } from "react-icons/ci";
+import { IoIosTimer } from "react-icons/io";
 const CreateInterview = () => {
   return <><section class="interview-Info">
   <div class="information">
@@ -10,20 +14,34 @@ const CreateInterview = () => {
     </div>
     <form class="form-fields">
       <div class="form-field">
+        <div className="label-icon">
+          <FaIdCard className="label-icon"/>
         <label>Name:</label>
+        </div>
       <input placeholder="Name" type="text" class="form-input"/>
       </div>
         <div class="form-field">
+        <div className="label-icon">
+          <MdOutlineMarkEmailRead/>
            <label>Email:</label>
+        </div>
+
         <input placeholder="Email" type="email" class="form-input"/>
       </div>
         <div class="form-field">
+        <div className="label-icon">
+           <FaRankingStar/>
            <label>Position:</label>
+        </div>
         <input placeholder="Position" type="text" class="form-input"/>
       </div>
       
         <div class="form-field">
+        <div className="label-icon">
+          <MdOutlineContactPage/>
            <label>Resume:</label>
+        </div>
+          
         <input placeholder="Resume" type="file" class="form-input"/>
       </div>
     </form>
@@ -34,19 +52,31 @@ const CreateInterview = () => {
     </div>
     <form class="form-fields">
       <div class="form-field">
+      <div className="label-icon">
+          <FaIdCard className="label-icon"/>
         <label>Name:</label>
+        </div>
       <input placeholder="Name" type="text" class="form-input"/>
       </div>
         <div class="form-field">
+        <div className="label-icon">
+          <MdOutlineMarkEmailRead/>
            <label>Email:</label>
+        </div>
         <input placeholder="Email" type="email" class="form-input"/>
       </div>
-        <div class="form-field">
+        <div class="form-field"> 
+           <div className="label-icon">
+          <CiCalendarDate/>
            <label>Interview Date:</label>
+        </div>
         <input placeholder="Date" type="date" class="form-input"/>
       </div>
       <div class="form-field">
-           <label>Interview TIme:</label>
+      <div className="label-icon">
+          <IoIosTimer/>
+           <label>Interview Time:</label>
+        </div>
         <input placeholder="Date" type="time" class="form-input"/>
       </div>
          

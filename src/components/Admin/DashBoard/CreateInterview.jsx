ba66a95bfe03d65f2f6553a8d551a7846d262271
nikhilkops <nikhil.kops@gmail.com>
2024-01-26 17:13:57 +0530

    added user Input for creating Interview

--- src/components/Admin/DashBoard/CreateInterview.jsx
+++ src/components/Admin/DashBoard/CreateInterview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import   './CreateInterview.css';
 import { FaIdCard } from "react-icons/fa";
 import { MdOutlineMarkEmailRead } from "react-icons/md";
@@ -6,7 +6,44 @@ import { FaRankingStar } from "react-icons/fa6";
 import { MdOutlineContactPage } from "react-icons/md";
 import { CiCalendarDate } from "react-icons/ci";
 import { IoIosTimer } from "react-icons/io";
-const CreateInterview = () => {
+const CreateInterview = () => { 
+  const [candidate, setCandidate] = useState({
+    name: "",
+    email: "",
+    position: "",
+    resume: ""
+  });
+
+  const [interviewer, setInterviewer] = useState({
+    name: "",
+    email: "",
+    date: "",
+    time: ""
+  });
+
+  // Event handler for updating Candidate details
+  const handleCandidateChange = (e) => {
+    const { name, value } = e.target;
+    setCandidate((prevCandidate) => ({
+      ...prevCandidate,
+      [name]: value
+    }));
+  };
+
+  const handleInterviewerChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value)
+    setInterviewer((prevCandidate) => ({
+      ...prevCandidate,
+      [name]: value
+    }));
+  };
+
+  const createInterview = ()=>{
+    console.log(candidate)
+    console.log(interviewer)
+  }
+
   return <><section class="interview-Info">
   <div class="information">
     <div class="form-heading">
@@ -18,7 +55,7 @@ const CreateInterview = () => {
           <FaIdCard className="label-icon"/>
         <label>Name:</label>
         </div>
-      <input placeholder="Name" type="text" class="form-input"/>
+      <input placeholder="Name" name="name" type="text" onChange={handleCandidateChange} class="form-input"/>
       </div>
         <div class="form-field">
         <div className="label-icon">
@@ -26,23 +63,22 @@ const CreateInterview = () => {
            <label>Email:</label>
         </div>
 
-        <input placeholder="Email" type="email" class="form-input"/>
+        <input placeholder="Email" name="email" type="email" onChange={handleCandidateChange} class="form-input"/>
       </div>
         <div class="form-field">
         <div className="label-icon">
            <FaRankingStar/>
            <label>Position:</label>
         </div>
-        <input placeholder="Position" type="text" class="form-input"/>
+        <input placeholder="Position" name="position" type="text" onChange={handleCandidateChange} class="form-input"/>
       </div>
       
         <div class="form-field">
         <div className="label-icon">
           <MdOutlineContactPage/>
            <label>Resume:</label>
-        </div>
-          
-        <input placeholder="Resume" type="file" class="form-input"/>
+        </div> 
+        <input placeholder="Resume" name="resume" type="file" onChange={handleCandidateChange} class="form-input"/>
       </div>
     </form>
   </div>
@@ -56,35 +92,35 @@ const CreateInterview = () => {
           <FaIdCard className="label-icon"/>
         <label>Name:</label>
         </div>
-      <input placeholder="Name" type="text" class="form-input"/>
+      <input placeholder="Name" name="name" onChange={handleInterviewerChange} type="text" class="form-input"/>
       </div>
         <div class="form-field">
         <div className="label-icon">
           <MdOutlineMarkEmailRead/>
            <label>Email:</label>
         </div>
-        <input placeholder="Email" type="email" class="form-input"/>
+        <input placeholder="Email" name="email" onChange={handleInterviewerChange} type="email" class="form-input"/>
       </div>
         <div class="form-field"> 
            <div className="label-icon">
           <CiCalendarDate/>
            <label>Interview Date:</label>
         </div>
-        <input placeholder="Date" type="date" class="form-input"/>
+        <input placeholder="Date" name="date" onChange={handleInterviewerChange} min={new Date().toISOString().split('T')[0]} type="date" class="form-input"/>
       </div>
       <div class="form-field">
       <div className="label-icon">
           <IoIosTimer/>
            <label>Interview Time:</label>
         </div>
-        <input placeholder="Date" type="time" class="form-input"/>
+        <input placeholder="Date" name="time" onChange={handleInterviewerChange} type="time" class="form-input"/>
       </div>
          
     </form>
   </div> 
   
 </section>
-<div className="create-btn-div">
+<div className="create-btn-div" onClick={createInterview}>
     <div className="btn create-interview">
       <span>Create Interview</span>
     </div>

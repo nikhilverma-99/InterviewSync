1e9218ace8e69bae747d2b6471ca26ed0ebd8f0c
nikhilkops <nikhil.kops@gmail.com>
2024-01-27 16:06:47 +0530

    static name to Amazon

--- src/Axios.js
+++ src/Axios.js
@@ -35,6 +35,6 @@ export const createInterview = async (formData)=>{
 }
 
 export const getAllInterview = async()=>{
-    let res = await API.post('/interview/get',{company:"Example Corp"})   
+    let res = await API.post('/interview/get',{company:"Amazon"})   
     return res;
 }

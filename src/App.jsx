aef0d731d691697596b41cbd0e4d68593c83a58e
nikhilkops <nikhil.kops@gmail.com>
2024-02-02 00:39:11 +0530

    Lazy Loading in Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import { useEffect, useState, createContext, useContext,lazy,Suspense  } from 'react'
-import NavBar from './components/NavBar/NavBar'
-import Hero from './components/Hero/Hero'  
 import './App.css'
-import './mediaquery.css'
-// import Editor from './components/CodeEditor/Editor'
+import './mediaquery.css' 
+const LandingPage = lazy(()=>import('./components/LandingPage/LandingPage'))  
 import Loading from './components/Loading/Loading'
 const TextEditor = lazy(()=> import('./components/QuestionTextEditor/TextEditor')) 
 const ProblemEditor = lazy(()=>import('./components/Problem+Editor/ProblemEditor'))
-const Login = lazy(()=>import('./components/Login/Login'))
-// import Table from './components/Table/Table'
-const Error = lazy(()=> import('./components/Error/Error')) 
-// import MaterialTable from './components/Table/MaterialTable'
+const Login = lazy(()=>import('./components/Login/Login')) 
+const Error = lazy(()=> import('./components/Error/Error'))  
 const Register = lazy(()=>import('./components/register/Register'))  
 const Admin  =lazy(()=> import('./components/Admin/Admin')) 
 import { Route, Routes } from 'react-router-dom'
-import VideoCall from './components/Dyanamic Width Components/VideoCall'
-import HowItWorks from './components/How it Works/HowItWorks'
+import VideoCall from './components/Dyanamic Width Components/VideoCall' 
 import Dashboard from './components/Admin/DashBoard/Dashboard'
 import CreateInterview from './components/Admin/DashBoard/CreateInterview'
 import AllInterview from './components/Admin/DashBoard/AllInterview'
 const UserLogin  = lazy(()=>import('./components/UserLogin/UserLogin')) 
 // import Adjustable from './components/Adjustable/Adjustable'
-
-
-import Pricing from './components/pricing/Pricing'
+ 
 const CodeCollabContext = createContext();
  
 function App() { 
@@ -34,12 +27,7 @@ function App() {
       <Suspense fallback={<Loading></Loading>}>
       <Routes>
             <Route   path='/' element={
-                <> 
-                <NavBar></NavBar>
-                <Hero></Hero>
-                <HowItWorks></HowItWorks>  
-                <Pricing></Pricing>
-                </>
+               <LandingPage></LandingPage>
             }/> 
             <Route path='enterInterview' element={<Login></Login>}></Route>
             <Route path='videocalling' element={<VideoCall></VideoCall>}></Route>  
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+import Hero from "../Hero/Hero";
+import HowItWorks from "../How it Works/HowItWorks";
+import Pricing from "../pricing/Pricing";
+const LandingPage = () => {
+  return  <main>
+    <NavBar></NavBar>
+    <Hero></Hero>
+    <HowItWorks></HowItWorks>
+    <Pricing></Pricing>
+  </main>
+};
+
+export default LandingPage;

d7db942fd4ae7c6c6ce1e940ef2d2ba2cdb6a154
nikhilkops <nikhil.kops@gmail.com>
2024-02-03 00:16:07 +0530

    new user login form made

--- src/components/UserLogin/UserLogin.jsx
+++ src/components/UserLogin/UserLogin.jsx
@@ -1,43 +1,49 @@
 import React from "react";
 import './UserLogin.css'
+import { NavLink } from "react-router-dom";
+import loginCartoon from '../../images/loginPage.webp'
+import Logo from '../../images/Logo.svg';
 const UserLogin = () => {
-  return <section class='login-popup-container'>
-  <div class='login-popup'>
-    <div class='popup-left'> 
-      
-      <header class='left-header'>
-        <figure>
-          <img src='https://interviewsync.in/assets/LightLogo-Zon_Ywxe.svg'  class='left-header-logo'/>
-        </figure>
-      </header> 
-       
-    </div>
-    <div class='popup-right'>
-      
-      <div class='rightHeading'>
-       <h1>Login</h1>
-        <p>Welcome back!  </p>
-        <p>Sign in to continue your seamless experience.</p>
-      </div>
-      
-      
-      <form class='login-form'>
-        <div class='form-registration-input'>
-          <label> Email</label>
-          <input type='text' class='registration-input'/>
-        </div>
-        <div class='form-registration-input'>
-          <label>  Password</label>
-          <input type='password' class='registration-input'/>
-        </div>
-        
-         <div class='registration-btn'>
+  return <section class='login-container'>
+       <div className="login-left">
+        <header className="login-left-header">
+          <figure className="header-image">
+            <img src={Logo} alt="Logo"/>
+          </figure>
+        </header>
+        <div className="login-heading">
           <span>Login</span>
         </div>
-      </form>
-    </div> 
-  </div>
-</section>
+        <form className="login-form">
+          <div className='form-login-input'>
+            <label> Email</label>
+            <input type='text' className='login-input' name='businessEmail' />
+          </div>
+          <div className='form-login-input'>
+            <label> Password</label>
+            <input type='password' className='login-input' name='businessEmail' />
+          </div>
+          <div className='login-btn' >
+            <span>Login</span>
+          </div>
+        </form>
+        <div className="other-option">
+          <NavLink className='otherOption-link' to='/forgotPassword'>Forgot Password</NavLink>
+          <NavLink className='otherOption-link'  to='/register'>Register</NavLink>
+
+        </div>
+       </div>
+       <div className="login-right">
+        <article>
+          <p> Welcome Back !</p>
+          <span>Sign in to continue your seamless experience.</span>
+        </article>
+        <figure className="login-cartoon-container">
+          <img src={loginCartoon} alt='Interview Sync Cartoon' className="cartoon-img"/>
+        </figure>
+       </div>
+
+      </section>
 };
 
 export default UserLogin;

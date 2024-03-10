314d691bc4778fd42d2d271cb6ebca8a3662bd03
nikhilkops <nikhil.kops@gmail.com>
2024-03-10 13:20:41 +0530

    get Starrted button working

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import './Pricing.css';
+import "./Pricing.css";
 
+import { useNavigate } from "react-router-dom";
 import { BsPeopleFill } from "react-icons/bs";
 import { BiSupport } from "react-icons/bi";
 import { GiTimeSynchronization } from "react-icons/gi";
 import { IoAnalyticsSharp } from "react-icons/io5";
 
-const icons = [<BsPeopleFill/>,<BiSupport/>,<GiTimeSynchronization/>,<IoAnalyticsSharp/>]
+const icons = [
+  <BsPeopleFill />,
+  <BiSupport />,
+  <GiTimeSynchronization />,
+  <IoAnalyticsSharp />,
+];
 const Pricing = () => {
+  const navigate = useNavigate();
   return (
     <section className="section-pricing">
       <div className="container">
@@ -15,77 +22,83 @@ const Pricing = () => {
         <h2 className="heading-secondary">
           Elevate Your Synced Interviews with Cost Effective Solutions.
         </h2>
-
       </div>
 
       <div className="pricing-container">
-      <div className="pricing-grid grid--3-cols"> 
-        
-        <article className="planPricing">
-          <header className="planHeader">
-            <p className="planName">Starter</p>
-            <p className="planPrice">$19</p>
-            <p className="planTag">/per month. That's just $4/Interview</p>
-          </header>
-          <ul className="plan-details">
-            <li>  {icons[0]}5 Interviews/Month</li>
-            <li> {icons[1]}Priority Support</li>
-            <li>{icons[2]}Code Synchronization</li>
-            <li>{icons[3]}Analytics</li>
-          </ul>
-        <div className="getStarted-btn">
-          <div className="buyBtn">
-            <span> Get Started</span>
-          </div>
-        
-          </div>
-        </article>
+        <div className="pricing-grid grid--3-cols">
+          <article className="planPricing">
+            <header className="planHeader">
+              <p className="planName">Starter</p>
+              <p className="planPrice">$19</p>
+              <p className="planTag">/per month. That's just $4/Interview</p>
+            </header>
+            <ul className="plan-details">
+              <li> {icons[0]}5 Interviews/Month</li>
+              <li> {icons[1]}Priority Support</li>
+              <li>{icons[2]}Code Synchronization</li>
+              <li>{icons[3]}Analytics</li>
+            </ul>
+            <div
+              className="getStarted-btn"
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              <div className="buyBtn">
+                <span> Get Started</span>
+              </div>
+            </div>
+          </article>
 
-        <article className="planPricing mostPopular">
-          <header className="planHeader">
-            <p className="planName">Standard</p>
-            <p className="planPrice">$49</p>
-            <p className="planTag">/per month. That's just $2.50/Interview</p>
-          </header>
-          <ul className="plan-details  ">
-            <li>  {icons[0]}20 Interviews/Month</li>
-            <li> {icons[1]}Priority Support</li>
-            <li>{icons[2]}Code Synchronization</li>
-            <li>{icons[3]}Analytics</li>
-          </ul>
-        <div className="getStarted-btn btn-mostPopular">
-          <div className="buyBtn">
-            <span> Get Started</span>
-          </div>
-        
-          </div>
-        </article>
+          <article className="planPricing mostPopular">
+            <header className="planHeader">
+              <p className="planName">Standard</p>
+              <p className="planPrice">$49</p>
+              <p className="planTag">/per month. That's just $2.50/Interview</p>
+            </header>
+            <ul className="plan-details  ">
+              <li> {icons[0]}20 Interviews/Month</li>
+              <li> {icons[1]}Priority Support</li>
+              <li>{icons[2]}Code Synchronization</li>
+              <li>{icons[3]}Analytics</li>
+            </ul>
+            <div
+              className="getStarted-btn btn-mostPopular"
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              <div className="buyBtn">
+                <span> Get Started</span>
+              </div>
+            </div>
+          </article>
 
-        <article className="planPricing premium">
-          <header className="planHeader">
-            <p className="planName">Premium</p>
-            <p className="planPrice">$99</p>
-            <p className="planTag">/per month. That's just $2.0/Interview</p>
-          </header>
-          <ul className="plan-details">
-            <li>  {icons[0]}50 Interviews/Month</li>
-            <li> {icons[1]}Priority Support</li>
-            <li>{icons[2]}Code Synchronization</li>
-            <li>{icons[3]}Analytics</li>
-          </ul>
-        <div className="getStarted-btn">
-          <div className="buyBtn">
-            <span> Get Started</span>
-          </div>
-        
-          </div>
-        </article>
-
+          <article className="planPricing premium">
+            <header className="planHeader">
+              <p className="planName">Premium</p>
+              <p className="planPrice">$99</p>
+              <p className="planTag">/per month. That's just $2.0/Interview</p>
+            </header>
+            <ul className="plan-details">
+              <li> {icons[0]}50 Interviews/Month</li>
+              <li> {icons[1]}Priority Support</li>
+              <li>{icons[2]}Code Synchronization</li>
+              <li>{icons[3]}Analytics</li>
+            </ul>
+            <div
+              className="getStarted-btn"
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              <div className="buyBtn">
+                <span> Get Started</span>
+              </div>
+            </div>
+          </article>
+        </div>
       </div>
-
-      </div>
-
-      
     </section>
   );
 };

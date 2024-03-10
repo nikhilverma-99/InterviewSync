985a3d49146f06b295258759db575d856e9a9470
nikhilkops <nikhil.kops@gmail.com>
2024-03-10 11:57:56 +0530

    corrected credential parsing in nterinterview

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,52 +2,52 @@ import React, { useEffect } from "react";
 import "./login.css";
 // import { ToastContainer, toast } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
-import axios from 'axios' 
+import axios from "axios";
 import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
 import { useState } from "react";
-import { Bars } from "react-loading-icons"; 
-import logo from "../../images/Logo.svg"; 
-import socket from "../../socket"
-import * as api from "../../Axios"
-import Loading from '../../images/Loading.gif'
-function Login() { 
+import { Bars } from "react-loading-icons";
+import logo from "../../images/Logo.svg";
+import socket from "../../socket";
+import * as api from "../../Axios";
+import Loading from "../../images/Loading.gif";
+function Login() {
   let [email, setEmail] = useState("");
   let [token, setToken] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const params = useSearchParams();
-  
-  async function LoginButton() {  
-      setLoading(true)
-      try{  
-        let user_email = email.split("/")[0]
-        let res = await api.joinInterview({email:user_email, roomID:token})
-        let type = res.data.type;
-        console.log(res) 
-        if(res.status==200){
-          const interviewId = res?.data?.invObj?._id;
-          localStorage.setItem("roomID",res.data.roomID)
-          console.log(localStorage.getItem("roomID"));
-          
-          socket.emit('joininterview',res.data.roomID) 
-          if(type=="I"){  
-            const resumeLink = res?.data?.invObj?.resume_url; 
-            navigate(`/questionSelect?_id=${interviewId}&resumeUrl=${btoa(resumeLink)}`) 
-          }else if(type=="C"){
-            console.log(res)
-            navigate(`/interviewLobby?_id=${interviewId}`); 
-          }else{
-            alert("please enter your type")
-          }   
-        }else{
-          console.log(res)
+
+  async function LoginButton() {
+    setLoading(true);
+    try {
+      let user_email = email.split("/")[0];
+      let res = await api.joinInterview({ email: user_email, roomID: token });
+      let type = res.data.type;
+      console.log(res);
+      if (res.status == 200) {
+        const interviewId = res?.data?.invObj?._id;
+        localStorage.setItem("roomID", res.data.roomID);
+        console.log(localStorage.getItem("roomID"));
+
+        socket.emit("joininterview", res.data.roomID);
+        if (type == "I") {
+          const resumeLink = res?.data?.invObj?.resume_url;
+          navigate(
+            `/questionSelect?_id=${interviewId}&resumeUrl=${btoa(resumeLink)}`
+          );
+        } else if (type == "C") {
+          console.log(res);
+          navigate(`/interviewLobby?_id=${interviewId}`);
+        } else {
+          alert("please enter your type");
         }
+      } else {
+        console.log(res);
       }
-      catch(e)
-      {
-        console.log(e)
-      }
-      setLoading(false)
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   }
 
   function fSetEmail(e) {
@@ -58,16 +58,14 @@ function Login() {
     let value = e.target.value;
     setToken(value);
   }
-  
-  useEffect(()=>{
-    
-    console.log(params[0].get("data"))
-    const credentials = params[0]?.get("data");
-    credentials = atob(JSON.parse(credentials))
-    console.log(credentials)
 
-    // setEmail(credentials.email);
-    // setToken(credentials.token) ;
+  useEffect(() => {
+    console.log(params[0].get("data"));
+    const credentials = JSON.parse(atob(params[0]?.get("data")));
+    console.log(credentials);
+
+    setEmail(credentials.email);
+    setToken(credentials.token) ;
     // if (currentUser) {
     //   toast.success(` User is already logged in !`, {
     //     position: toast.POSITION.TOP_CENTER,
@@ -75,7 +73,7 @@ function Login() {
     //     style: {
     //       fontSize: '18px', // Set the desired font size
     //     }
-    
+
     // if (currentUser) {
     //   toast.success(` User is already logged in !`, {
     //     position: toast.POSITION.TOP_CENTER,
@@ -84,71 +82,70 @@ function Login() {
     //       fontSize: '18px', // Set the desired font size
     //     }
     //   });
-  
+
     //   navigate("/")
     // }
     // }
-  })
+  });
   return (
     <div id="loginContainer">
-    <div class="authorize-background"></div>
-    <div class="container-login">
+      <div class="authorize-background"></div>
+      <div class="container-login">
         <div class="image_container">
-            <img class="logo-container" alt="Logo" src={logo} loading="lazy" />
+          <img class="logo-container" alt="Logo" src={logo} loading="lazy" />
 
-            <a href="/">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    class="close"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
-                    />
-                </svg>
-            </a>
+          <a href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              class="close"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </a>
         </div>
 
         <form class="login_info">
-            <div class="email">
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={fSetEmail}
-                    autoComplete="on"
-                />
-            </div>
-            <div class="password">
-                <input 
-                    placeholder="Token"
-                    value={token}
-                    onChange={fSetToken}
-                    autoComplete="on"
-                />
-            </div>
+          <div class="email">
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={fSetEmail}
+              autoComplete="on"
+            />
+          </div>
+          <div class="password">
+            <input
+              placeholder="Token"
+              value={token}
+              onChange={fSetToken}
+              autoComplete="on"
+            />
+          </div>
 
-            {loading ? (
-               <div class="login_button_container" >
-                    <div class="login_button">
-                        <img style={{height:'50%'}} src={Loading}/>
-                    </div>
-                </div> 
-            ) : (
-                <div class="login_button_container" onClick={LoginButton}>
-                    <div class="login_button">
-                        <span>Join Interview</span>
-                    </div>
-                </div> 
-            )}
+          {loading ? (
+            <div class="login_button_container">
+              <div class="login_button">
+                <img style={{ height: "50%" }} src={Loading} />
+              </div>
+            </div>
+          ) : (
+            <div class="login_button_container" onClick={LoginButton}>
+              <div class="login_button">
+                <span>Join Interview</span>
+              </div>
+            </div>
+          )}
         </form>
+      </div>
     </div>
-</div>
-
   );
 }
 
